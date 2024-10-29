562d0b45f28aaba376c9ebab12c16a5538ce02b1
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-10-29 08:35:21 -0600

    feat(config): cors.ts was added

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express  from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
+import { corsOptions } from "./config/cors";
+import cors  from "cors";
 
 dotenv.config();
 
@@ -10,6 +12,8 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 // NOTE: Routes
 app.use('/api/projects', projectRoutes);
